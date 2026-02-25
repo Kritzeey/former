f88888ca88d065ce7a7218c56089e3bac2969a3c
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-02-25 12:21:35 +0700

    feat: add create user use-case

---
+++ apps/server/src/application/use-cases/create-user.use-case.ts
@@ -0,0 +1,32 @@
+import { User } from "@/domain/entities/user.entity";
+import type { IPasswordHasher } from "../ports/password-hasher.interface";
+import type { IUserRepository } from "../ports/user-repository.interface";
+import { randomUUID } from "crypto";
+
+export class CreateUserUseCase {
+  constructor(
+    private userRepository: IUserRepository,
+    private passwordHasher: IPasswordHasher,
+  ) {}
+
+  async execute(
+    username: string,
+    plainTextPassword: string,
+  ): Promise<Omit<User, "passwordHash">> {
+    const existingUser = await this.userRepository.findByUsername(username);
+
+    if (existingUser) {
+      throw new Error("Username is already registered");
+    }
+
+    const hashedPassword = await this.passwordHasher.hash(plainTextPassword);
+
+    const user = new User(randomUUID(), username, hashedPassword, new Date());
+
+    await this.userRepository.save(user);
+
+    const { passwordHash, ...result } = user;
+
+    return result;
+  }
+}
