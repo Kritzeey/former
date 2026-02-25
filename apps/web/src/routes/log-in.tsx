b839d249691ee53afb13ec1e3d9aa29ee5ce85ef
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-02-25 19:13:24 +0700

    feat: add log in functionality

--- apps/web/src/routes/log-in.tsx
+++ apps/web/src/routes/log-in.tsx
@@ -8,9 +8,70 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { z } from "zod";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+
+export const logInSchema = z.object({
+  username: z.string().min(1, "Username is required"),
+  password: z.string().min(1, "Password is required"),
+});
+
+export type LogInDto = z.infer<typeof logInSchema>;
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<LogInDto>({
+    resolver: zodResolver(logInSchema),
+  });
+
+  const onSubmit = async (data: LogInDto) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/log-in", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        const errorMessage = errorData.message || "";
+
+        const match = errorMessage.match(/^\[Err:\s*([^\]]+)\]\s*(.*)$/);
+
+        if (match) {
+          const field = match[1] as keyof LogInDto;
+          const message = match[2];
+
+          setError(field, { type: "server", message });
+          return;
+        }
+
+        throw new Error(errorMessage || "Something went wrong during log in.");
+      }
+
+      toast.success("Logged in successfully!");
+      navigate("/");
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <main className="w-full h-dvh flex items-center justify-center">
       <Card className="w-full max-w-md shadow-lg rounded-md">
@@ -27,30 +88,72 @@ export default function Login() {
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="username">Username</Label>
-            <Input
-              className="rounded-md placeholder:text-muted-foreground/60"
-              id="text"
-              type="text"
-              placeholder="johndoe"
-            />
-          </div>
+        <CardContent>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="username"
+                className={errors.username ? "text-red-500" : ""}
+              >
+                Username
+              </Label>
+              <Input
+                className={
+                  errors.username
+                    ? "border-red-500 focus-visible:ring-red-500 rounded-md placeholder:text-muted-foreground/60"
+                    : "rounded-md placeholder:text-muted-foreground/60"
+                }
+                id="username"
+                type="text"
+                placeholder="johndoe"
+                disabled={isLoading}
+                {...register("username")}
+              />
+              {errors.username && (
+                <p className="text-sm text-red-500 font-medium">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
 
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              className="rounded-md placeholder:text-muted-foreground/60"
-              id="password"
-              type="password"
-              placeholder="●●●●●●●●"
-            />
-          </div>
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="password"
+                className={errors.password ? "text-red-500" : ""}
+              >
+                Password
+              </Label>
+              <Input
+                className={
+                  errors.password
+                    ? "border-red-500 focus-visible:ring-red-500 rounded-md placeholder:text-muted-foreground/60"
+                    : "rounded-md placeholder:text-muted-foreground/60"
+                }
+                id="password"
+                type="password"
+                placeholder="●●●●●●●●"
+                disabled={isLoading}
+                {...register("password")}
+              />
+              {errors.password && (
+                <p className="text-sm text-red-500 font-medium">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
 
-          <Button className="rounded-md w-full mt-2 hover:opacity-90 cursor-pointer">
-            Log In
-          </Button>
+            <Button
+              className="rounded-md w-full mt-2 hover:opacity-90 cursor-pointer"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              {isLoading ? "Logging in..." : "Log In"}
+            </Button>
+          </form>
         </CardContent>
       </Card>
     </main>
